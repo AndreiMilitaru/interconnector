4924a7515906ee6b9fe85d81408597698a670668
AndreiMilitaru <andrei.militaru@gmail.com>
2026-03-11 12:49:48 +0100

    added a decimation filter in the utilities, for quick smoothing of curves

--- src/interconnector/analysis/__init__.py
+++ src/interconnector/analysis/__init__.py
@@ -6,7 +6,7 @@ from .cavity_log_analysis import CavityLogAnalyzer
 from .interferometer_log_analysis import InterferometerLogAnalyzer
 from .adaptive_lock_log_analysis import AdaptiveLockLogAnalyzer
 from .photon_counting_analysis import PhotonCountingAnalyzer, ZBasisPhotonCountingAnalyzer
-from .utilities import analyze_below_threshold, transfer_mask, bootstrap_timetrace, summarize_bootstrap
+from .utilities import analyze_below_threshold, transfer_mask, bootstrap_timetrace, summarize_bootstrap, decimate
 
 __all__ = ['CavityLogAnalyzer', 'InterferometerLogAnalyzer', 'AdaptiveLockLogAnalyzer',
            'PhotonCountingAnalyzer', 'ZBasisPhotonCountingAnalyzer',
